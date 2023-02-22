7f1062bd95fa59757b2503289deba5a36ba15469
charline-studi <studi@charline-laporte.fr>
2023-02-22 15:03:53 +0100

    Ajout de la base HTML + CSS pour la session bonus

---
+++ live-bonus/script.js
@@ -0,0 +1,15 @@
+console.log('V1 : Mon dico anglais')
+
+/*
+
+MON PROGRAMME : 
+
+> Je veux pouvoir donner la définition d'un mot à mes utilisateurs
+
+- Récupérer le mot saisi par l'utilisateur
+- Envoyer le mot à l'API ( https://dictionaryapi.dev/ )
+- Récupérer la définition du mot
+- Afficher la définition du mot
+- Ajouter un lecteur pour écouter la prononciation du mot
+
+*/
