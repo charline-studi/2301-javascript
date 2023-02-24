27b4438d2278de9c4a1a934dafb9788c4f0af770
charline-studi <studi@charline-laporte.fr>
2023-02-24 16:04:21 +0100

    Ajout cours bonus 2

--- live-bonus/script.js
+++ live-bonus/script.js
@@ -29,18 +29,52 @@ const watchSubmit = () => {
 
 /* ÉTAPE 2 : Envoyer mon mot à l'API */
 const apiCall = (word) => {
-    console.log("WORD TO SEARCH", word)
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-        .then((response) => response.json())
+        .then((response) => response.json()) // Javascript Object NotatiON
         .then((data) => {
             /* ÉTAPE 3 : RÉCUPÉRER LA DONNÉE */
-            const wordInformations = data[0]
-            console.log("Mot", wordInformations.word)
+            const informationsNeeded = extractData(data[0])
+            renderToHTML(informationsNeeded)
+        })
+        .catch((error) => {
+            alert('Le mot demandé n\'existe pas')
+            console.error(error)
         })
 }
 
+const extractData = (data) => {
+    // 1 - Mot 
+    const word = data.word
+    // 2 - Écriture phonétique
+    const phonetic = findProp(data.phonetics, "text")
+    // 3 - Prononciation (audio)
+    const pronoun = findProp(data.phonetics, "audio")
+    // 4 - Définition(s)
+    const meanings = data.meanings
 
+    return {
+        word: word,
+        phonetic: phonetic,
+        pronoun: pronoun, 
+        meanings: meanings
+    }
+}
 
+const findProp = (array, name) => {
+    // Elle parcours un tableau d'objets
+    for (let i = 0; i < array.length; i++) {
+        // Et cherche dans ce tableau, si l'objet en cours contient une certaines propriété
+        const currentObject = array[i]
+        const hasProp = currentObject.hasOwnProperty(name)
+        // Alors elle renvoit cette propriété
+        if (hasProp) return currentObject[name]
+    }
+}
+
+/* ÉTAPE 4 : Afficher les informations de mon mot sur ma page */
+const renderToHTML = () => {
+    console.log('AJOUTER LES INFOS SUR MA PAGE HTML')
+}
 
 
 
