3a51cb02ced271c171a7e7ba4531cbc1c5339100
charline-studi <studi@charline-laporte.fr>
2023-03-02 16:17:43 +0100

    Ajout du cours bonus 3

--- live-bonus/script.js
+++ live-bonus/script.js
@@ -72,10 +72,60 @@ const findProp = (array, name) => {
 }
 
 /* ÉTAPE 4 : Afficher les informations de mon mot sur ma page */
-const renderToHTML = () => {
-    console.log('AJOUTER LES INFOS SUR MA PAGE HTML')
-}
+const renderToHTML = (data) => {
+    const card = document.querySelector('.js-card')
+    card.classList.remove('card--hidden')
 
+    // Manipulation de textes avec la propriété textContent
+    const title = document.querySelector(".js-card-title")
+    title.textContent = data.word
+    const phonetic = document.querySelector(".js-card-phonetic")
+    phonetic.textContent = data.phonetic
+    
+    // Création d'élements HTML dynamiques
+    const list = document.querySelector('.js-card-list')
+    list.innerHTML = ""
+    for(let i = 0; i < data.meanings.length; i++) {
+        const meaning = data.meanings[i]
+        const partOfSpeech = meaning.partOfSpeech
+        const definition = meaning.definitions[0].definition
+
+        // 1 - Avec un innerHTML
+        // list.innerHTML += `
+        // <li class="card__meaning">
+        //     <p class="card__part-of-speech">${partOfSpeech}</p>
+        //     <p class="card__definition">${definition}</p>
+        // </li>`
+        // Attention : lisibilité peut être mauvaise quand on a de gros blocs HTML
+
+        // 2 - Avec la création d'élements 
+        const li = document.createElement('li')
+        li.classList.add('card__meaning')
+        const pPartOfSpeech = document.createElement('p')
+        pPartOfSpeech.textContent = partOfSpeech
+        pPartOfSpeech.classList.add('card__part-of-speech')
+        const pDefinition = document.createElement('p')
+        pDefinition.textContent = definition
+        pDefinition.classList.add('card__definition')
+        
+        li.appendChild(pPartOfSpeech)
+        li.appendChild(pDefinition)
+        list.appendChild(li)
+    }
+
+    // Ajout de l'audio en JS
+    const button = document.querySelector('.js-card-button')
+    const audio = new Audio(data.pronoun)
+    button.addEventListener('click', () => {
+        button.classList.remove("card__player--off")
+        button.classList.add("card__player--on")
+        audio.play()
+    })
+    audio.addEventListener('ended', () => {
+        button.classList.remove("card__player--on")
+        button.classList.add("card__player--off")
+    })
+}
 
 
 // LANCEMENT DU PROGRAMME
